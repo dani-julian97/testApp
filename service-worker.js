6c6780b23d2b8dfff198929f76ff4eb9b6d452f6
djulian <djulian@seqeuntiabiotech.com>
2026-07-27 17:29:27 +0200

    Initial commit

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,35 @@
-const CACHE_NAME = "mi-app-v1";
+const CACHE_NAME = "pulse-app-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
+  "./styles.css",
   "./manifest.json",
   "./icon-192.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
